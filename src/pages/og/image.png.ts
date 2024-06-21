5bbc45b2f9a5306f395ec67be4169afa3a372514
rpdeshaies <rpdeshaies@gmail.com>
2024-06-21 15:39:13 -0400

    index image

--- src/pages/og/image.png.ts
+++ src/pages/og/image.png.ts
@@ -1,10 +1,19 @@
 import type { APIRoute } from "astro";
+import { getCollection } from "astro:content";
 import { renderOgImage } from "../../domains/og-image/renderOgImage";
 
 export const GET: APIRoute = async ({ params, request }) => {
+  const numberOfCreators = (await getCollection("creators")).length;
+  const numberOfGames = (await getCollection("games")).length;
+  const numberOfAssets = (await getCollection("assets")).length;
+
   return await renderOgImage({
     title: "Keeper",
     description: "The best way to play your favorite TTRPGs",
-    // footerItems: ["Hello", "World"],
+    // footerItems: [
+    //   `${numberOfCreators} creators`,
+    //   `${numberOfGames} games`,
+    //   `${numberOfAssets} assets`,
+    // ],
   });
 };
