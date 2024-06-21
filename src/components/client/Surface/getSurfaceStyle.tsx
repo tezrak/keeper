f03426965670db6f37df6adcacb02ee2a663f184
rpdeshaies <rpdeshaies@gmail.com>
2024-06-21 10:35:12 -0400

    fix: surface style

--- src/components/client/Surface/getSurfaceStyle.tsx
+++ src/components/client/Surface/getSurfaceStyle.tsx
@@ -1,8 +1,8 @@
 export function getSurfaceStyle(): React.CSSProperties {
   return {
     background:
-      "linear-gradient(137deg,rgba(17,18,20,.75) 4.87%,rgba(12,13,15,.9) 75.88%)",
-    boxShadow: "inset 0 1px 1px 0 hsla(0,0%,100%,.15)",
+      "linear-gradient(132deg, rgb(from var(--accent-2) r g b / 40%) 15%, rgb(from var(--accent-1) r g b / 60%))",
+    boxShadow: "inset 0 1px 1px 0 rgba(255, 255, 255, 0.15)",
     backdropFilter: "blur(5px)",
   };
 }
